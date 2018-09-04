81abacc26d6c6179acf74194c2a0a9f1cbb5e25f
shadowolf <shadowolfbusiness@gmail.com>
2018-09-04 15:17:43 +1000

    Added a Math.floor presence to handler.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const client = require('discord-rich-presence')('484662509765132289');
+const client = require('discord-rich-presence')('486399845251416067');
 
 client.on('connected', () => {
-  console.log('DiscordRP Client connected!');
+  function setPresence() {
+    presenceDisplay = [
+      	['Connected to Nebula01', 'Destroying the Monument', 'avicus_large', null],
+      	['Connected to Development', 'Developing things', 'avicus_large', null],
+        ['Browsing the Forums', 'Handling Appeals', 'avicus_large', null],
+        ['Connected to Mix01', 'Moderating the Server', 'avicus_large', null],
+    ];
 
-  client.updatePresence({
-    state: 'Connected to Lobby01',
-    details: 'Casually waiting in Lobby',
-    startTimestamp: new Date(),
-    largeImageKey: 'avicus_large',
-    smallImageKey: 'avicus',
-  });
+    x = Math.floor(Math.random()*presenceDisplay.length);
+
+    client.updatePresence({
+      state: `${presenceDisplay[x][0]}`,
+      details: `${presenceDisplay[x][1]}`,
+      startTimestamp: new Date(),
+      largeImageKey: `${presenceDisplay[x][2]}`,
+      smallImageKey: `${presenceDisplay[x][3]}`,
+    });
+    console.log(`\n\n[Console] Presence has been changed\nState: ${presenceDisplay[x][0]}\nDetails: ${presenceDisplay[x][1]}\nLarge Image Key: ${presenceDisplay[x][2]}\nSmall Image Key: ${presenceDisplay[x][3]}`);
+  };
+
+  setInterval(setPresence, 600000);
+  console.log('DiscordRP Client connected!');
 });
 
 process.on('unhandledRejection', console.error);
